3bf7907753083c5ed1fe640f7a53cf29507c8a03
Bence Magyar <bence.magyar@gmail.com>
2013-03-23 17:09:37 -0400

    turned off asset precompile

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,10 @@ module Googlemap
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #disable for heroku
+    config.assets.initialize_on_precompile = false
+
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
