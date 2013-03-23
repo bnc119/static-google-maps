efc1d2744842190a72bfdfa3d65b337af79889e8
Bence Magyar <bence.magyar@gmail.com>
2013-03-23 13:04:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Googlemap::Application.config.secret_token = '01fd3e5911bca94ca450460d054b913c4b94c3589d25f3bff8382bfd85c10ca454def1295b693188b408e6fe7c8e7c9e96ca424cef0f37de024b503ffb680d39'
